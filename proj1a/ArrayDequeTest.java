9e3087b42a066827198427917224ea75e58fddbb
Haochao Zhu <zhuhaochao@gmail.com>
2018-09-02 21:37:55 +1000

    pro1a completed

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,8 +1,29 @@
 public class ArrayDequeTest {
 
-    public static void manin(String[] args) {
+    public static void main(String[] args) {
 
-        int[] a = {0,1,2,3,4,5,6,7};
+        ArrayDeque<Integer> a = new ArrayDeque();
+
+        a.addFirst(21);
+        a.addFirst(43);
+        a.addLast(212);
+        a.addFirst(63);
+        a.addLast(212);
+        a.addFirst(45);
+        a.addLast(67);
+        a.addFirst(12);
+        a.addFirst(53);
+
+        a.removeLast();
+        a.removeFirst();
+        a.removeLast();
+        a.removeFirst();
+        a.removeFirst();
+        a.removeFirst();
+
+        a.printDeque();
+
+
 
     }
 
