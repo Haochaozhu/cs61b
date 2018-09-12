ab16776bc31c9f50f42353dedd9ba688767d7ef3
Haochao Zhu <zhuhaochao@gmail.com>
2018-09-12 20:06:52 +1000

    HW5 completed

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,5 +1,9 @@
 package synthesizer;
 import org.junit.Test;
+
+import java.net.Inet4Address;
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /** Tests the ArrayRingBuffer class.
@@ -9,11 +13,29 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
+        arb.enqueue(10);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(9);
+
+        for (int i: arb) {
+            System.out.println(i);
+        }
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+//        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
+        arb.enqueue(10);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(9);
+
+        for (Integer i: arb) {
+            System.out.println(i);
+        }
     }
 } 
